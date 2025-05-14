dba0e61d3bf35ef7d80d411b64284a331f833df9
Ayzeld <130386759+Ayzeld@users.noreply.github.com>
2025-05-14 11:13:51 +0700

    Update DZ2.cpp

--- С++/DZ2.cpp
+++ С++/DZ2.cpp
@@ -2,9 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
-#include <cmath>
 
-// Заполнение массивов
 void FillInc(int A[], int n) {
     for (int i = 0; i < n; i++) A[i] = i;
 }
@@ -17,7 +15,6 @@ void FillRand(int A[], int n) {
     for (int i = 0; i < n; i++) A[i] = rand() % 1000;
 }
 
-// Пузырьковая сортировка с правильным подсчётом
 void BubbleSort(int A[], int n, int& comparisons, int& moves) {
     comparisons = 0;
     moves = 0;
@@ -35,70 +32,48 @@ void BubbleSort(int A[], int n, int& comparisons, int& moves) {
     }
 }
 
-// Вывод таблицы с теоретическими значениями
-void PrintTheoreticalTable() {
-    std::cout << "\nТЕОРЕТИЧЕСКИЕ ЗНАЧЕНИЯ (M + C):\n";
-    std::cout << "+------+---------------+---------------+---------------+\n";
-    std::cout << "|  N   |    Min (M=0)  | Средний случай | Max (M=3*C)   |\n";
-    std::cout << "+------+---------------+---------------+---------------+\n";
-    
-    int sizes[] = {100, 200, 300, 400, 500};
-    for (int n : sizes) {
-        double C = (pow(n, 2) - n)/2;
-        double M_min = 0;
-        double M_avg = 3 * ((pow(n, 2) - n)/4;
-        double M_max = 3 * ((pow(n, 2) - n)/2);
-        
-        std::cout << "| " << std::setw(4) << n << " | " 
-                  << std::setw(12) << (C + M_min) << " | "
-                  << std::setw(14) << (C + M_avg) << " | "
-                  << std::setw(12) << (C + M_max) << " |\n";
-        std::cout << "+------+---------------+---------------+---------------+\n";
-    }
-}
+void PrintTable() {
+    std::cout << "+------+----------------+---------------------+" << std::endl;
+    std::cout << "|  N   | M+C теоретич.  |    Мфакт+Сфакт      |" << std::endl;
+    std::cout << "|      |                | Убыв. | Случ. | Возр. |" << std::endl;
+    std::cout << "+------+----------------+-------+-------+-------+" << std::endl;
 
-// Вывод таблицы с практическими результатами
-void PrintPracticalTable() {
-    std::cout << "\nПРАКТИЧЕСКИЕ РЕЗУЛЬТАТЫ (M + C):\n";
-    std::cout << "+------+---------------+---------------+---------------+\n";
-    std::cout << "|  N   | Возр. (min)  | Случайный      | Убыв. (max)   |\n";
-    std::cout << "+------+---------------+---------------+---------------+\n";
-    
     int sizes[] = {100, 200, 300, 400, 500};
     for (int n : sizes) {
         int* A = new int[n];
         int C, M;
         
-        // Лучший случай (возрастающий)
-        FillInc(A, n);
-        BubbleSort(A, n, C, M);
-        int min = C + M;
+        // Теоретическое значение для худшего случая (M = 3C)
+        int theoretical = 2 * ((n*n - n)/2); // C + M = C + 3C = 4C, но формула требует уточнения
         
-        // Средний случай (случайный)
-        FillRand(A, n);
-        BubbleSort(A, n, C, M);
-        int avg = C + M;
-        
-        // Худший случай (убывающий)
+        // Убывающий (максимальные M+C)
         FillDec(A, n);
         BubbleSort(A, n, C, M);
-        int max = C + M;
+        int dec_total = C + M;
         
+        // Случайный
+        FillRand(A, n);
+        BubbleSort(A, n, C, M);
+        int rand_total = C + M;
+        
+        // Возрастающий (минимальные M+C)
+        FillInc(A, n);
+        BubbleSort(A, n, C, M);
+        int inc_total = C + M;
+
         std::cout << "| " << std::setw(4) << n << " | " 
-                  << std::setw(12) << min << " | "
-                  << std::setw(14) << avg << " | "
-                  << std::setw(12) << max << " |\n";
-        std::cout << "+------+---------------+---------------+---------------+\n";
-        
+                  << std::setw(14) << (3 * (n*n - n)/2 + (n*n - n)/2) << " | "
+                  << std::setw(5) << dec_total << " | "
+                  << std::setw(5) << rand_total << " | "
+                  << std::setw(5) << inc_total << " |" << std::endl;
+        std::cout << "+------+----------------+-------+-------+-------+" << std::endl;
+
         delete[] A;
     }
 }
 
 int main() {
     srand(time(0));
-    
-    PrintTheoreticalTable();
-    PrintPracticalTable();
-    
+    PrintTable();
     return 0;
 }
