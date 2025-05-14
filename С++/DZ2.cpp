179b67996a64ec6b89856d081daafc5c3e3f0e84
Ayzeld <130386759+Ayzeld@users.noreply.github.com>
2025-05-14 11:10:09 +0700

    Update DZ2.cpp

--- С++/DZ2.cpp
+++ С++/DZ2.cpp
@@ -1,108 +1,104 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
+#include <cmath>
 
+// Заполнение массивов
 void FillInc(int A[], int n) {
-    for (int i = 0; i < n; i++) {
-        A[i] = i;
-    }
+    for (int i = 0; i < n; i++) A[i] = i;
 }
 
 void FillDec(int A[], int n) {
-    for (int i = 0; i < n; i++) {
-        A[i] = n - i - 1;
-    }
+    for (int i = 0; i < n; i++) A[i] = n - i - 1;
 }
 
 void FillRand(int A[], int n) {
-    for (int i = 0; i < n; i++) {
-        A[i] = rand() % 100;
-    }
+    for (int i = 0; i < n; i++) A[i] = rand() % 1000;
 }
 
-int CheckSum(int A[], int n) {
-    int summ = 0;
-    for (int i = 0; i < n; i++) {
-        summ += A[i];
-    }
-    return summ;
-}
-
-int RunNumber(int A[], int n) {
-    if (n <= 1) {
-        return n;
-    }
-    int series = 1;
-    for (int i = 1; i < n; i++) {
-        if (A[i] != A[i - 1]) {
-            series++;
-        }
-    }
-    return series;
-}
-
-void PrintMas(int A[], int n) {
-    for (int i = 0; i < n; i++) {
-        std::cout << A[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
-void bubbleSort(int A[], int n, int& comparisons, int& swaps) {
-    comparisons = 0; // счетчики 
-    swaps = 0;
+// Пузырьковая сортировка с правильным подсчётом
+void BubbleSort(int A[], int n, int& comparisons, int& moves) {
+    comparisons = 0;
+    moves = 0;
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             comparisons++;
             if (A[j] > A[j + 1]) {
-                std::swap(A[j], A[j + 1]);
-                swaps++;
+                // 3 пересылки при обмене
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+                moves += 3;
             }
         }
     }
 }
 
+// Вывод таблицы с теоретическими значениями
+void PrintTheoreticalTable() {
+    std::cout << "\nТЕОРЕТИЧЕСКИЕ ЗНАЧЕНИЯ (M + C):\n";
+    std::cout << "+------+---------------+---------------+---------------+\n";
+    std::cout << "|  N   |    Min (M=0)  | Средний случай | Max (M=3*C)   |\n";
+    std::cout << "+------+---------------+---------------+---------------+\n";
+    
+    int sizes[] = {100, 200, 300, 400, 500};
+    for (int n : sizes) {
+        double C = (pow(n, 2) - n)/2;
+        double M_min = 0;
+        double M_avg = 3 * ((pow(n, 2) - n)/4;
+        double M_max = 3 * ((pow(n, 2) - n)/2);
+        
+        std::cout << "| " << std::setw(4) << n << " | " 
+                  << std::setw(12) << (C + M_min) << " | "
+                  << std::setw(14) << (C + M_avg) << " | "
+                  << std::setw(12) << (C + M_max) << " |\n";
+        std::cout << "+------+---------------+---------------+---------------+\n";
+    }
+}
+
+// Вывод таблицы с практическими результатами
+void PrintPracticalTable() {
+    std::cout << "\nПРАКТИЧЕСКИЕ РЕЗУЛЬТАТЫ (M + C):\n";
+    std::cout << "+------+---------------+---------------+---------------+\n";
+    std::cout << "|  N   | Возр. (min)  | Случайный      | Убыв. (max)   |\n";
+    std::cout << "+------+---------------+---------------+---------------+\n";
+    
+    int sizes[] = {100, 200, 300, 400, 500};
+    for (int n : sizes) {
+        int* A = new int[n];
+        int C, M;
+        
+        // Лучший случай (возрастающий)
+        FillInc(A, n);
+        BubbleSort(A, n, C, M);
+        int min = C + M;
+        
+        // Средний случай (случайный)
+        FillRand(A, n);
+        BubbleSort(A, n, C, M);
+        int avg = C + M;
+        
+        // Худший случай (убывающий)
+        FillDec(A, n);
+        BubbleSort(A, n, C, M);
+        int max = C + M;
+        
+        std::cout << "| " << std::setw(4) << n << " | " 
+                  << std::setw(12) << min << " | "
+                  << std::setw(14) << avg << " | "
+                  << std::setw(12) << max << " |\n";
+        std::cout << "+------+---------------+---------------+---------------+\n";
+        
+        delete[] A;
+    }
+}
+
 int main() {
-    const int n = 15; // Размер массива
-    int A[n];
-    int M, C; // Переменные для подсчета пересылок и сравнений
-    srand(static_cast<unsigned>(time(0))); // Инициализируем генератор случайных чисел
-
-    // Возрастающий массив
-    std::cout << "Возрастающий массив: ";
-    FillInc(A, n);
-    PrintMas(A, n);
-    M = C = 0; // Сбрасываем счетчики
-    bubbleSort(A, n, M, C);
-    std::cout << "После bubbleSort: ";
-    PrintMas(A, n);
-    std::cout << "Пересылки (M): " << M << ", Сравнения (C): " << C << std::endl;
-    std::cout << "Серия: " << RunNumber(A, n) << std::endl;
-    std::cout << "Контрольная сумма: " << CheckSum(A, n) << std::endl;
-
-    // Убывающий массив
-    std::cout << "Убывающий массив: ";
-    FillDec(A, n);
-    PrintMas(A, n);
-    M = C = 0; // Сбрасываем счетчики
-    bubbleSort(A, n, M, C);
-    std::cout << "После bubbleSort: ";
-    PrintMas(A, n);
-    std::cout << "Пересылки (M): " << M << ", Сравнения (C): " << C << std::endl;
-    std::cout << "Серия: " << RunNumber(A, n) << std::endl;
-    std::cout << "Контрольная сумма: " << CheckSum(A, n) << std::endl;
-
-    // Случайный массив
-    std::cout << "Рандомный массив: ";
-    FillRand(A, n);
-    PrintMas(A, n);
-    M = C = 0; // Сбрасываем счетчики
-    bubbleSort(A, n, M, C);
-    std::cout << "После bubbleSort: ";
-    PrintMas(A, n);
-    std::cout << "Пересылки (M): " << M << ", Сравнения (C): " << C << std::endl;
-    std::cout << "Серия: " << RunNumber(A, n) << std::endl;
-    std::cout << "Контрольная сумма: " << CheckSum(A, n) << std::endl;
-
-    return 0; // Возвращаем 0, чтобы указать на успешное выполнение
+    srand(time(0));
+    
+    PrintTheoreticalTable();
+    PrintPracticalTable();
+    
+    return 0;
 }
