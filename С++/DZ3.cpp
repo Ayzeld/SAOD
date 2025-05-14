9865a2bddbfd6a783162ec81576bfecbf736b807
Ayzeld <130386759+Ayzeld@users.noreply.github.com>
2025-05-14 11:43:02 +0700

    Update DZ3.cpp

--- С++/DZ3.cpp
+++ С++/DZ3.cpp
@@ -26,7 +26,7 @@ void ShakerSort(int A[], int n, int& M, int& C) {
             C++;
             if (A[i] > A[i + 1]) {
                 std::swap(A[i], A[i + 1]);
-                M += 3;
+                M += 3; // 3 пересылки на каждый обмен
                 swapped = true;
             }
         }
@@ -37,7 +37,7 @@ void ShakerSort(int A[], int n, int& M, int& C) {
             C++;
             if (A[i] < A[i - 1]) {
                 std::swap(A[i], A[i - 1]);
-                M += 3;
+                M += 3; // 3 пересылки на каждый обмен
                 swapped = true;
             }
         }
@@ -46,18 +46,15 @@ void ShakerSort(int A[], int n, int& M, int& C) {
 }
 
 void PrintTable() {
-    std::cout << "+------+----------------+---------------------+" << std::endl;
-    std::cout << "|  N   | M+C теоретич.  |    Мфакт+Сфакт      |" << std::endl;
-    std::cout << "|      |                | Убыв. | Случ. | Возр. |" << std::endl;
-    std::cout << "+------+----------------+-------+-------+-------+" << std::endl;
+    std::cout << "+------+---------------------+" << std::endl;
+    std::cout << "|  N   |    Мфакт+Сфакт      |" << std::endl;
+    std::cout << "|      | Убыв. | Случ. | Возр. |" << std::endl;
+    std::cout << "+------+-------+-------+-------+" << std::endl;
 
     int sizes[] = {100, 200, 300, 400, 500};
     for (int n : sizes) {
         int* A = new int[n];
         int M, C;
-        
-        // Теоретическое значение (аналогично пузырьковой)
-        int theoretical = 2 * (n*n - n); // Примерная оценка
         
         // Убывающий (максимальные M+C)
         FillDec(A, n);
@@ -75,11 +72,10 @@ void PrintTable() {
         int inc_total = M + C;
 
         std::cout << "| " << std::setw(4) << n << " | " 
-                  << std::setw(14) << theoretical << " | "
                   << std::setw(5) << dec_total << " | "
                   << std::setw(5) << rand_total << " | "
                   << std::setw(5) << inc_total << " |" << std::endl;
-        std::cout << "+------+----------------+-------+-------+-------+" << std::endl;
+        std::cout << "+------+-------+-------+-------+" << std::endl;
 
         delete[] A;
     }
@@ -87,21 +83,6 @@ void PrintTable() {
 
 int main() {
     srand(static_cast<unsigned>(time(0)));
-    
-    // Демонстрация для маленького массива
-    const int demo_n = 10;
-    int demo_A[demo_n];
-    int M, C;
-    
-    std::cout << "Демонстрация ShakerSort для n=" << demo_n << ":\n";
-    
-    std::cout << "Убывающий массив:\n";
-    FillDec(demo_A, demo_n);
-    ShakerSort(demo_A, demo_n, M, C);
-    std::cout << "M=" << M << " C=" << C << " M+C=" << M+C << "\n\n";
-    
-    // Основная таблица
     PrintTable();
-    
     return 0;
 }
