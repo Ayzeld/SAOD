c9ebd729aa85fad10908aeb517ad0e0fa116fa60
Ayzeld <130386759+Ayzeld@users.noreply.github.com>
2025-05-21 10:43:02 +0700

    Update DZ3.cpp

--- С++/DZ3.cpp
+++ С++/DZ3.cpp
@@ -15,7 +15,21 @@ void FillRand(int A[], int n) {
     for (int i = 0; i < n; i++) A[i] = rand() % 100;
 }
 
-void ShakerSort(int A[], int n, int& M, int& C) {
+void BubbleSort(int A[], int n, int& M, int& C) {
+    M = 0;
+    C = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            C++; // сравнение
+            if (A[j] > A[j + 1]) {
+                std::swap(A[j], A[j + 1]);
+                M += 3; // три пересылки при обмене
+            }
+        }
+    }
+}
+
+void ShakerSort(int A[], int n, int& M, int C) {
     int left = 0, right = n - 1;
     bool swapped = true;
     M = 0;
@@ -26,7 +40,7 @@ void ShakerSort(int A[], int n, int& M, int& C) {
             C++;
             if (A[i] > A[i + 1]) {
                 std::swap(A[i], A[i + 1]);
-                M += 3; // 3 пересылки на каждый обмен
+                M += 3;
                 swapped = true;
             }
         }
@@ -37,7 +51,7 @@ void ShakerSort(int A[], int n, int& M, int& C) {
             C++;
             if (A[i] < A[i - 1]) {
                 std::swap(A[i], A[i - 1]);
-                M += 3; // 3 пересылки на каждый обмен
+                M += 3;
                 swapped = true;
             }
         }
@@ -46,36 +60,61 @@ void ShakerSort(int A[], int n, int& M, int& C) {
 }
 
 void PrintTable() {
-    std::cout << "+------+---------------------+" << std::endl;
-    std::cout << "|  N   |    Мфакт+Сфакт      |" << std::endl;
-    std::cout << "|      | Убыв. | Случ. | Возр. |" << std::endl;
-    std::cout << "+------+-------+-------+-------+" << std::endl;
+    std::cout << "+------+---------------------+---------------------+\n";
+    std::cout << "|  N   |     Пузырьковая     |      Шейкерная      |\n";
+    std::cout << "|      | Убыв. | Случ. | Возр. | Убыв. | Случ. | Возр. |\n";
+    std::cout << "+------+-------+-------+-------+-------+-------+-------+\n";
 
     int sizes[] = {100, 200, 300, 400, 500};
+    const int num_sizes = sizeof(sizes) / sizeof(sizes[0]);
+
     for (int n : sizes) {
         int* A = new int[n];
-        int M, C;
-        
-        // Убывающий (максимальные M+C)
+
+        int M_bubble, C_bubble;
+        int M_shaker, C_shaker;
+
+        // Пузырьковая
+        // Убывающий
         FillDec(A, n);
-        ShakerSort(A, n, M, C);
-        int dec_total = M + C;
-        
+        BubbleSort(A, n, M_bubble, C_bubble);
+        int dec_bubble = M_bubble + C_bubble;
+
         // Случайный
         FillRand(A, n);
-        ShakerSort(A, n, M, C);
-        int rand_total = M + C;
-        
-        // Возрастающий (минимальные M+C)
-        FillInc(A, n);
-        ShakerSort(A, n, M, C);
-        int inc_total = M + C;
+        BubbleSort(A, n, M_bubble, C_bubble);
+        int rand_bubble = M_bubble + C_bubble;
 
-        std::cout << "| " << std::setw(4) << n << " | " 
-                  << std::setw(5) << dec_total << " | "
-                  << std::setw(5) << rand_total << " | "
-                  << std::setw(5) << inc_total << " |" << std::endl;
-        std::cout << "+------+-------+-------+-------+" << std::endl;
+        // Возрастающий
+        FillInc(A, n);
+        BubbleSort(A, n, M_bubble, C_bubble);
+        int inc_bubble = M_bubble + C_bubble;
+
+        // Шейкерная
+        // Убывающий
+        FillDec(A, n);
+        ShakerSort(A, n, M_shaker, C_shaker);
+        int dec_shaker = M_shaker + C_shaker;
+
+        // Случайный
+        FillRand(A, n);
+        ShakerSort(A, n, M_shaker, C_shaker);
+        int rand_shaker = M_shaker + C_shaker;
+
+        // Возрастающий
+        FillInc(A, n);
+        ShakerSort(A, n, M_shaker, C_shaker);
+        int inc_shaker = M_shaker + C_shaker;
+
+        std::cout << "| " << std::setw(4) << n << " | "
+                  << std::setw(5) << dec_bubble << " | "
+                  << std::setw(5) << rand_bubble << " | "
+                  << std::setw(5) << inc_bubble << " | "
+                  << std::setw(5) << dec_shaker << " | "
+                  << std::setw(5) << rand_shaker << " | "
+                  << std::setw(5) << inc_shaker << " |" << std::endl;
+
+        std::cout << "+------+-------+-------+-------+-------+-------+-------+\n";
 
         delete[] A;
     }
