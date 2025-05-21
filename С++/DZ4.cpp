22d1c9acd7f72cb2993a43203a2e6374822a7c0d
Ayzeld <130386759+Ayzeld@users.noreply.github.com>
2025-05-21 10:51:35 +0700

    Update DZ4.cpp

--- С++/DZ4.cpp
+++ С++/DZ4.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
 
-// Заполнение массива возрастающими числами
+// Функции заполнения массива
 void FillInc(int A[], int n) {
     for (int i = 0; i < n; i++) {
         A[i] = i;
     }
 }
 
-// Заполнение массива убывающими числами
 void FillDec(int A[], int n) {
     for (int i = 0; i < n; i++) {
         A[i] = n - i - 1;
     }
 }
 
-// Заполнение массива случайными числами
 void FillRand(int A[], int n) {
     for (int i = 0; i < n; i++) {
         A[i] = rand() % 100;
@@ -69,52 +66,59 @@ void InsertSort(int A[], int n, int& comparisons, int& swaps) {
     }
 }
 
-// Вывод массива
-void PrintArray(int A[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << A[i] << " ";
-    }
-    cout << endl;
+// Вывод таблицы
+void PrintTableHeader() {
+    cout << "+------+----------------+---------------------+" << endl;
+    cout << "|  N   | M+C теоретич.  | Mфакт+Сфакт         |" << endl;
+    cout << "|      |                | Убыв. | Случ. | Возр. |" << endl;
+    cout << "+------+----------------+-------+-------+-------+" << endl;
+}
+
+void PrintTableRow(int n, int theoretical, int dec_fact, int rand_fact, int inc_fact) {
+    cout << "| " << setw(4) << n << " | "
+         << setw(14) << theoretical << " | "
+         << setw(5) << dec_fact << " | "
+         << setw(5) << rand_fact << " | "
+         << setw(5) << inc_fact << " |" << endl;
+    cout << "+------+----------------+-------+-------+-------+" << endl;
 }
 
 int main() {
-    const int n = 15;
-    int A[n];
-    int comparisons, swaps;
+    const int sizes[] = {100, 200, 300, 400, 500};
+    const int num_sizes = sizeof(sizes) / sizeof(sizes[0]);
     srand(time(NULL));
+    PrintTableHeader();
 
-    // Возрастающий массив
-    cout << "Возрастающий массив: ";
-    FillInc(A, n);
-    PrintArray(A, n);
-    InsertSort(A, n, comparisons, swaps);
-    cout << "Отсортированный массив: ";
-    PrintArray(A, n);
-    cout << "Сравнения: " << comparisons << ", Пересылки: " << swaps << endl;
-    cout << "Серия: " << RunNumber(A, n) << endl;
-    cout << "Контрольная сумма: " << CheckSum(A, n) << endl;
+    for (int i = 0; i < num_sizes; i++) {
+        int n = sizes[i];
+        int* A = new int[n];
 
-    // Убывающий массив
-    cout << "Убывающий массив: ";
-    FillDec(A, n);
-    PrintArray(A, n);
-    InsertSort(A, n, comparisons, swaps);
-    cout << "Отсортированный массив: ";
-    PrintArray(A, n);
-    cout << "Сравнения: " << comparisons << ", Пересылки: " << swaps << endl;
-    cout << "Серия: " << RunNumber(A, n) << endl;
-    cout << "Контрольная сумма: " << CheckSum(A, n) << endl;
+        // Теоретическое значение
+        int theoretical_min = 3 * (n - 1); // Для возрастающего массива
+        int theoretical_max = n * n + n - 2; // Для убывающего массива
+        int theoretical_avg = (n * n - n) / 2 + n - 1; // Для случайного массива
 
-    // Случайный массив
-    cout << "Случайный массив: ";
-    FillRand(A, n);
-    PrintArray(A, n);
-    InsertSort(A, n, comparisons, swaps);
-    cout << "Отсортированный массив: ";
-    PrintArray(A, n);
-    cout << "Сравнения: " << comparisons << ", Пересылки: " << swaps << endl;
-    cout << "Серия: " << RunNumber(A, n) << endl;
-    cout << "Контрольная сумма: " << CheckSum(A, n) << endl;
+        // Для убывающего массива
+        FillDec(A, n);
+        int comparisons_dec, swaps_dec;
+        InsertSort(A, n, comparisons_dec, swaps_dec);
+        int fact_dec = comparisons_dec + swaps_dec;
 
+        // Для случайного массива
+        FillRand(A, n);
+        int comparisons_rand, swaps_rand;
+        InsertSort(A, n, comparisons_rand, swaps_rand);
+        int fact_rand = comparisons_rand + swaps_rand;
+
+        // Для возрастающего массива
+        FillInc(A, n);
+        int comparisons_inc, swaps_inc;
+        InsertSort(A, n, comparisons_inc, swaps_inc);
+        int fact_inc = comparisons_inc + swaps_inc;
+
+        // Вывод строки таблицы
+        PrintTableRow(n, theoretical_avg, fact_dec, fact_rand, fact_inc);
+        delete[] A;
+    }
     return 0;
 }
