d43eda3fe6e54fca0a56dcfc7153a2c911a1ce1c
Ayzeld <130386759+Ayzeld@users.noreply.github.com>
2025-05-22 12:43:20 +0700

    Update DZ8_Heapsort.cpp

--- С++/DZ8_Heapsort.cpp
+++ С++/DZ8_Heapsort.cpp
@@ -25,7 +25,7 @@ void FillRand(int A[], int n) {
     }
 }
 
-// Построение кучи
+// Построение кучи с подсчётом сравнений и обменов
 void heapify(int A[], int n, int i, int& comparisons, int& swaps) {
     int largest = i;
     int left = 2 * i + 1;
@@ -52,7 +52,16 @@ void heapify(int A[], int n, int i, int& comparisons, int& swaps) {
     }
 }
 
-// HeapSort
+// Функция только построения кучи (трудоёмкость построения пирамиды)
+void BuildHeap(int A[], int n, int& comparisons, int& swaps) {
+    comparisons = 0;
+    swaps = 0;
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(A, n, i, comparisons, swaps);
+    }
+}
+
+// Полная сортировка кучей (BuildHeap + сортировка)
 void HeapSort(int A[], int n, int& comparisons, int& swaps) {
     comparisons = 0;
     swaps = 0;
@@ -70,44 +79,79 @@ void HeapSort(int A[], int n, int& comparisons, int& swaps) {
     }
 }
 
-// Вывод таблицы
-void PrintTable() {
-    cout << "+------+----------------+-------+-------+-------+" << endl;
-    cout << "|  N   | M+C теоретич.  | Убыв. | Случ. | Возр. |" << endl;
-    cout << "+------+----------------+-------+-------+-------+" << endl;
+// Вывод таблицы с построением кучи
+void PrintBuildHeapTable() {
+    cout << "Таблица 1: Трудоёмкость построения кучи (BuildHeap)\n";
+    cout << "+------+--------------+-------+-------+-------+\n";
+    cout << "|  N   |  Теоретич.  | Убыв. | Случ. | Возр. |\n";
+    cout << "+------+--------------+-------+-------+-------+\n";
 
     int sizes[] = {100, 200, 300, 400, 500};
     for (int n : sizes) {
         int* A = new int[n];
 
-        // Убывающий массив
         FillDec(A, n);
-        int heap_comparisons_dec = 0, heap_swaps_dec = 0;
-        HeapSort(A, n, heap_comparisons_dec, heap_swaps_dec);
-        int total_heap_dec = heap_comparisons_dec + heap_swaps_dec;
+        int comp_dec = 0, swaps_dec = 0;
+        BuildHeap(A, n, comp_dec, swaps_dec);
+        int total_dec = comp_dec + swaps_dec;
 
-        // Случайный массив
         FillRand(A, n);
-        int heap_comparisons_rand = 0, heap_swaps_rand = 0;
-        HeapSort(A, n, heap_comparisons_rand, heap_swaps_rand);
-        int total_heap_rand = heap_comparisons_rand + heap_swaps_rand;
+        int comp_rand = 0, swaps_rand = 0;
+        BuildHeap(A, n, comp_rand, swaps_rand);
+        int total_rand = comp_rand + swaps_rand;
 
-        // Возрастающий массив
         FillInc(A, n);
-        int heap_comparisons_inc = 0, heap_swaps_inc = 0;
-        HeapSort(A, n, heap_comparisons_inc, heap_swaps_inc);
-        int total_heap_inc = heap_comparisons_inc + heap_swaps_inc;
+        int comp_inc = 0, swaps_inc = 0;
+        BuildHeap(A, n, comp_inc, swaps_inc);
+        int total_inc = comp_inc + swaps_inc;
 
-        // Теоретическая трудоёмкость
-        int theoretical_avg = static_cast<int>(n * log2(n) + 6.5 * n - 4);
+        int theoretical = static_cast<int>(n * log2(n) + 6.5 * n - 4);
 
-        // Вывод строки таблицы
         cout << "| " << setw(4) << n << " | "
-             << setw(14) << theoretical_avg << " | "
-             << setw(5) << total_heap_dec << " | "
-             << setw(5) << total_heap_rand << " | "
-             << setw(5) << total_heap_inc << " |" << endl;
-        cout << "+------+----------------+-------+-------+-------+" << endl;
+             << setw(12) << theoretical << " | "
+             << setw(5) << total_dec << " | "
+             << setw(5) << total_rand << " | "
+             << setw(5) << total_inc << " |\n";
+        cout << "+------+--------------+-------+-------+-------+\n";
+
+        delete[] A;
+    }
+}
+
+// Вывод таблицы с полной сортировкой
+void PrintHeapSortTable() {
+    cout << "\nТаблица 2: Трудоёмкость полной сортировки (HeapSort)\n";
+    cout << "+------+--------------+-------+-------+-------+\n";
+    cout << "|  N   |  Теоретич.  | Убыв. | Случ. | Возр. |\n";
+    cout << "+------+--------------+-------+-------+-------+\n";
+
+    int sizes[] = {100, 200, 300, 400, 500};
+    for (int n : sizes) {
+        int* A = new int[n];
+
+        FillDec(A, n);
+        int comp_dec = 0, swaps_dec = 0;
+        HeapSort(A, n, comp_dec, swaps_dec);
+        int total_dec = comp_dec + swaps_dec;
+
+        FillRand(A, n);
+        int comp_rand = 0, swaps_rand = 0;
+        HeapSort(A, n, comp_rand, swaps_rand);
+        int total_rand = comp_rand + swaps_rand;
+
+        FillInc(A, n);
+        int comp_inc = 0, swaps_inc = 0;
+        HeapSort(A, n, comp_inc, swaps_inc);
+        int total_inc = comp_inc + swaps_inc;
+
+        int theoretical = static_cast<int>(n * log2(n) + 6.5 * n - 4);
+
+        cout << "| " << setw(4) << n << " | "
+             << setw(12) << theoretical << " | "
+             << setw(5) << total_dec << " | "
+             << setw(5) << total_rand << " | "
+             << setw(5) << total_inc << " |\n";
+        cout << "+------+--------------+-------+-------+-------+\n";
 
         delete[] A;
     }
@@ -115,6 +159,9 @@ void PrintTable() {
 
 int main() {
     srand(static_cast<unsigned>(time(0)));
-    PrintTable();
+
+    PrintBuildHeapTable();
+    PrintHeapSortTable();
+
     return 0;
 }
